86aa4b6ac32b280cb69a26eee37acc575794dde8
bhardwajsaurav <saurav.bhardwaj@ekaleido.co>
2022-08-05 19:33:44 +0530

    update

--- js/scrollmagic.js
+++ js/scrollmagic.js
@@ -1,9 +1,27 @@
+const flightPath = {
+    curviness:1.25,
+    autoRoted:true,
+    value:[{x:50,y:80},{x:90,y:180},{x:50,y:80},{x:90,y:180}]
+ }
+const tween = new TimelineLite();
+
+tween.add(
+    TweenLite.to("img-vector",1,{
+         bezier:flightPath,
+         ease:Power1.easeInout
+    })
+)
+
 $(document).ready(function(){
     var controller = new ScrollMagic.Controller()
     var ourScene = new ScrollMagic.Scene({
-         triggerElement:"#vector_div"
+         triggerElement:"#vector_div",
+         duration:1000,
+         triggerHook: 0.5,
     })
-    .setClassToggle("#vector_div","fade-in")
+    .setClassToggle("#img-vector","fade-in")
+    .addIndicators()
+    .setPin("#vector_div")
     .addTo(controller)
 })
 

484b50b4fa5faecf47956777c22b1ebb3a33c087
Anuj kumar <anuj.kumar@ekaleido.co>
2022-08-17 13:09:18 +0530

    updated section 2

--- js/second_sec.js
+++ js/second_sec.js
@@ -1,27 +1,37 @@
-$(document).ready(function () {
-	var controller = new ScrollMagic.Controller();
-	var ourScene = new ScrollMagic.Scene({
-		triggerElement: ".second_sec",
-		duration: 1500,
-		triggerHook: 0.25,
+const ufo_tween = new TimelineLite();
+const ufo_path = {
+	curviness: 0,
+	autoRotate: true,
+	values: [
+		{x: 0, y: 100},
+		{x: -500, y: 100},
+		{x: 0, y: 100},
+		{x: 500, y: 100},
+		{x: 0, y: 100},
+	],
+};
+
+ufo_tween.add(
+	TweenLite.to(".ufo", 1, {
+		rotation: 0,
+		bezier: ufo_path,
+		ease: Power1.easeInOut,
 	})
-		.setClassToggle(".ufo", "ufo_scrollM")
-		.addTo(controller);
-});
+);
 
 $(document).ready(function () {
 	var controller = new ScrollMagic.Controller();
 	var ourScene = new ScrollMagic.Scene({
 		triggerElement: ".second_sec",
-		duration: 1000,
-		triggerHook: 0.25,
+		duration: 2000,
+		triggerHook: 0,
 	})
-		.setClassToggle(".rays", "height-ray")
+		.setTween(ufo_tween)
+		.setPin(".second_sec")
 		.addTo(controller);
 });
 
 const tween = new TimelineLite();
-
 tween.add(
 	TweenLite.to("#img_vector", 0.1, {
 		rotation: 360,
